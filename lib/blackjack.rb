e82494bddb8cd3d5deaa9fbee38dddd22d3d829a
aeisha simmons <ashicks2017@outlook.com>
2019-05-09 20:31:45 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -39,23 +39,22 @@ def initial_round
   
 end
 
-def hit?(current_card_total)
-  # code hit? here
-  prompt_user
-  input = get_user_input
-  
-  if input = 'h' || 'H'
-   current_card_total += deal_card
-   return current_card_total
-  
-  elsif input = 's' || 'S'
-   return current_card_total
-   
-  else 
+def hit?(card_total)
+  prompt_user #ask the user if they want to hit or stay
+  user_input = get_user_input #set a variable to use for your if/else statement
+  if user_input == "h"
+    card_total += deal_card #increase your former total by the new number given by dealing another card
+  elsif user_input == "s"
+    card_total #return the number and prompt user to hit again
+  else
     invalid_command
   end
+end
+
+def invalid_command
   puts "Please enter a valid command"
   prompt_user
+  get_user_input
 end
 
 def invalid_command
